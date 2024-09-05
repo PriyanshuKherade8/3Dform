9719b7d7114a1d46367a049a803e1072d5fb6c15
PriyanshuKherade8 <priyanshu.stark2@gmail.com>
2024-09-05 17:33:54 +0530

    fix: added router

--- src/index.js
+++ src/index.js
@@ -1,17 +1,45 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import './index.css';
+// import App from './App';
+// import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
+// // If you want to start measuring performance in your app, pass a function
+// // to log results (for example: reportWebVitals(console.log))
+// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
+
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+import AddForm from "./Pages/FormPage/AddForm";
+// import queryClient from "./queryClient";
+// import { QueryClientProvider } from "@tanstack/react-query";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <AddForm />,
+    // errorElement: <ErrorPage />,
+  },
+  {
+    path: "pageone",
+    element: <>{"hii"}</>,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    {/* <QueryClientProvider client={queryClient}> */}
+    <RouterProvider router={router} />
+    {/* </QueryClientProvider> */}
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
